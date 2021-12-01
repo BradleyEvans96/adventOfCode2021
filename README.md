bcef4425f2bb6cfc7b54dc47c5b6247fc5497f10
JoshPavBjss <josh.paveley@bjss.com>
2021-12-01 13:37:34 +0000

    Make dynamic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adventOfCode2021
+
+## Running the code
+
+Use `npm run solve` to execute the code
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import Day1 from "./day1/Day1";
 import { readFileForDay } from "./shared/fileToArray";
 
 // The day to run
 const dayNumber = 1
 
-// Read the input
-const input = readFileForDay(dayNumber)
+// Run the code
+solveForDay(dayNumber)
 
-// If you were testing using their inputs, you could just comment out the above line and do something like this
-// const input = [ 199, '200','208','210','200','207','240','269','260','263']
+async function solveForDay(dayNumber: number): Promise<void> {
+  // Read the input
+  const input = readFileForDay(dayNumber)
 
-console.log('🎄 Advent of Code 2021 🎁')
+  // If you were testing using their example inputs, you could just comment out the above line and do something like this
+  // Although adding unit testing would be better
+  // const input = [ 199, '200','208','210','200','207','240','269','260','263']
 
-const partOneAnswer = Day1.solvePartOne(input)
-console.log(`Part 1: ${partOneAnswer}`)
+  console.log('🎄 Advent of Code 2021 🎁')
+  console.log(`Day ${dayNumber}`)
 
-const partTwoAnswer = Day1.solvePartTwo(input)
-console.log(`Part 2: ${partTwoAnswer}`)
+  // Dynamically import the day from the file. This assumes the file and folder are named correctly. And the file exports a default Day
+  const { default: day } = await import(`./day${dayNumber}/Day${dayNumber}`)
+
+  const partOneAnswer = day.solvePartOne(input)
+  console.log(`Part 1: ${partOneAnswer}`)
+
+  const partTwoAnswer = day.solvePartTwo(input)
+  console.log(`Part 2: ${partTwoAnswer}`)
+}
