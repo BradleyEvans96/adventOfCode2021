d61f657d25627db6df9ece5eb6776e200bc4d383
Bradley Evans <bradley.evans@bjss.com>
2021-12-01 17:22:18 +0000

    Boilerplate for AOC

--- src/Day1/Day1.ts
+++ src/Day1/Day1.ts
@@ -2,23 +2,9 @@ import { Day } from '../shared/day';
 
 export default {
     solvePartOne: (input: string[]): string => {
-
-        let increaseCount = 0;
-
-        for (let i = 1; i < input.length; i++)
-        {
-            // console.log(input[i])
-            let j = i -1;
-            if (input[i] > input[j])
-            {
-                increaseCount ++;
-            }
-        }
-
-        return String(increaseCount)
+        return 'put answer here'
     },
     solvePartTwo: (input: string[]): string => {
-        // Write code here
-        return ''
+        return '';
     }
 } as Day
--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,8 @@ solveForDay(dayNumber)
 
 async function solveForDay(dayNumber: number): Promise<void> {
   // Read the input
-  const testinput = readTestFileForDay(dayNumber)
-  const input = readFileForDay(dayNumber)
-
+  const testinput = readFileForDay(dayNumber)
+  const exampleinput = readTestFileForDay(dayNumber)
   // If you were testing using their example inputs, you could just comment out the above line and do something like this
   // Although adding unit testing would be better
   // const input = [ 199, '200','208','210','200','207','240','269','260','263']
@@ -21,15 +20,15 @@ async function solveForDay(dayNumber: number): Promise<void> {
   // Dynamically import the day from the file. This assumes the file and folder are named correctly. And the file exports a default Day
   const { default: day } = await import(`./day${dayNumber}/Day${dayNumber}`)
 
-  const partOneExampleAnswer = day.solvePartOne(testinput)
-  console.log(`Part 1: ${partOneExampleAnswer}`)
+  const partOneExampleAnswer = day.solvePartOne(exampleinput)
+  console.log(`Part 1 Example Answer: ${partOneExampleAnswer}`)
 
-  const partOneAnswer = day.solvePartOne(input)
-  console.log(`Part 1: ${partOneAnswer}`)
+  const partOneAnswer = day.solvePartOne(testinput)
+  console.log(`Part 1 Actual Answer: ${partOneAnswer}`)
 
-  const partTwoExampleAnswer = day.solvePartTwo(testinput)
-  console.log(`Part 2: ${partTwoExampleAnswer}`)
+  const partTwoExampleAnswer = day.solvePartTwo(exampleinput)
+  console.log(`Part 2 Example Answer: ${partTwoExampleAnswer}`)
 
-  const partTwoAnswer = day.solvePartTwo(input)
-  console.log(`Part 2: ${partTwoAnswer}`)
+  const partTwoAnswer = day.solvePartTwo(testinput)
+  console.log(`Part 2 Actual Answer: ${partTwoAnswer}`)
 }
