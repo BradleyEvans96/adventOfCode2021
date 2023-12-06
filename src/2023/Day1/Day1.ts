b979684fa6a4230e04ffe9b857c09399886ad255
Bradley Evans <bradley.evans@bjss.com>
2023-12-06 16:35:18 +0000

    Add Solution for day 1

--- src/2023/Day1/Day1.ts
+++ src/2023/Day1/Day1.ts
@@ -1,12 +1,45 @@
+import { sumArrayOfNumbers } from "../../helpers/reducers";
 import { Day } from "../../shared/day";
 
 export const solvePartOne = (input: string[]) => {
-  console.log("input - ", input);
-  return "";
+  const reg = new RegExp("[0-9]");
+  const numbers = input.map((line) =>
+    line.split("").filter((l) => reg.test(l))
+  );
+  const firstAndLast = numbers
+    .map((num) => Number(`${num[0]}${num[num.length - 1]}`))
+    .filter((v) => !Number.isNaN(v));
+  return String(firstAndLast.reduce(sumArrayOfNumbers, 0));
+};
+
+const getWordsAndNumbers = (input: string) => {
+  const reg = new RegExp(
+    /[0-9]|(?=(one|two|three|four|five|six|seven|eight|nine))/g
+  );
+  return Array.from(input.matchAll(reg), ([g1, g2]) => [...g1, g2])
+    .flatMap((v) => v)
+    .filter((v) => v && v !== "");
 };
 
 export const solvePartTwo = (input: string[]) => {
-  return "";
+  const wordToNumber = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+  const numbers = input
+    .map((line) => getWordsAndNumbers(line))
+    .map((line) => line.map((v) => (wordToNumber[v] ? wordToNumber[v] : v)));
+  const firstAndLast = numbers
+    .map((num) => Number(`${num[0]}${num[num.length - 1]}`))
+    .filter((v) => !Number.isNaN(v));
+  return String(firstAndLast.reduce(sumArrayOfNumbers, 0));
 };
 
 export default {
